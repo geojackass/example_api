7f8290f29f98533e2190bfbae570d9e511e4de13
Takahiro Tsuchida <tsuchida@engraphia.com>
2013-10-17 22:40:28 +0900

    Commit init.sql

---
+++ init.sql
@@ -0,0 +1,13 @@
+DROP TABLE earth_observation_data;
+CREATE TABLE earth_observation_data(
+id serial NOT NULL,
+lat NUMERIC(6,3),
+lon NUMERIC(6,3),
+prc NUMERIC(6,3),
+sst NUMERIC(5,3),
+ssw NUMERIC(4,2),
+smc NUMERIC(6,3),
+snd NUMERIC(6,3),
+sic NUMERIC(6,3),
+CONSTRAINT earth_observation_data_key PRIMARY KEY (id)
+);
