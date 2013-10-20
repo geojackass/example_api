1efc049d6b33c743d18d73925e86b195e3e9d39e
Takahiro Tsuchida <tsuchida@engraphia.com>
2013-10-20 22:20:57 +0900

    値が存在しない時の代用値を入れるための桁数に変更。

--- init.sql
+++ init.sql
@@ -2,13 +2,13 @@ DROP TABLE earth_observation_data;
 CREATE TABLE earth_observation_data(
 id serial NOT NULL,
 observed_at DATE,
-lat NUMERIC(6,3),
-lon NUMERIC(6,3),
-prc NUMERIC(6,3),
-sst NUMERIC(5,3),
-ssw NUMERIC(4,2),
-smc NUMERIC(6,3),
-snd NUMERIC(6,3),
-sic NUMERIC(6,3),
+lat NUMERIC(7,3),
+lon NUMERIC(7,3),
+prc NUMERIC(7,3),
+sst NUMERIC(7,3),
+ssw NUMERIC(7,3),
+smc NUMERIC(7,3),
+snd NUMERIC(7,3),
+sic NUMERIC(7,3),
 CONSTRAINT earth_observation_data_key PRIMARY KEY (id)
 );
