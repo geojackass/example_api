f78361266c664fc15370c6c3202a550759baaf0d
Takahiro Tsuchida <tsuchida@engraphia.com>
2013-10-22 23:08:36 +0900

    インデックスおよびインデックス用カラムの追加。

--- init.sql
+++ init.sql
@@ -10,5 +10,13 @@ ssw NUMERIC(7,3),
 smc NUMERIC(7,3),
 snd NUMERIC(7,3),
 sic NUMERIC(7,3),
+observed_at_year INTEGER,
+observed_at_month INTEGER,
+observed_at_day INTEGER,
 CONSTRAINT earth_observation_data_key PRIMARY KEY (id)
 );
+CREATE INDEX earth_observation_data_lat_index ON earth_observation_data(lat);
+CREATE INDEX earth_observation_data_lon_index ON earth_observation_data(lon);
+CREATE INDEX earth_observation_data_observed_at_year_index ON earth_observation_data(observed_at_year);
+CREATE INDEX earth_observation_data_observed_at_month_index ON earth_observation_data(observed_at_month);
+CREATE INDEX earth_observation_data_observed_at_day_index ON earth_observation_data(observed_at_day);
